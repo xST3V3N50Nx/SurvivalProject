28fb8d27d8bfb00ad65350cf245742f43b1f1615
DESKTOP-S49KVM2\Andy <andys1995@outlook.com>
2020-08-20 01:06:13 +0100

    Commit Notes: - Imported Standard Assets - Setup DevelopmentRoom01(Scene) - Setup Basic Character Controller - Created Player.CS Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstIncreaseRate, hungerIncreaseRate;
+    private float health, thirst, hunger;
+
+    public bool playerDead;
+    //Functions
+    public void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void Update()
+    {
+
+        //Thirst and Hunger increase
+        if(!playerDead)
+        {
+            thirst += thirstIncreaseRate * Time.deltaTime;
+            hunger += hungerIncreaseRate * Time.deltaTime;
+        }
+
+        //Change to reduce HP over time
+        if (thirst >= maxThirst)
+            Die();
+        if (thirst >= maxHunger)
+            Die();
+
+    }
+
+    public void Die()
+    {
+        //Player Death Event(Change to HP)
+        playerDead = true;
+        print("You Have Died From Malnutrition");
+    }
+
+}
